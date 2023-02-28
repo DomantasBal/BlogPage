3406d1889ae011af8015a3245fda80295896c208
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-28 10:52:47 +0200

    API and posting content part

--- app/api.js
+++ app/api.js
@@ -1,27 +1,45 @@
+// BLOG
+const blog = document.querySelector("#blog");
 
-getPosts();
+// FORM
+const newPostForm = document.querySelector("#new-post-form");
+// MAKE POST BTN
+const makePostBtn = document.querySelector("#submit-new-post");
 
-function getPosts (){
-    fetch("	https://testapi.io/api/Domantas/resource/Posts")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => console.log(err))
-}
+// POST TITLE VALUE
+const postTitle = document.querySelector("#post-title");
+// POST TEXT VALUE
+const postText = document.querySelector("#post-text");
+// POST URL VALUE
+const postImg = document.querySelector("#post-img");
 
 
 
-function postPost() {
+// MAKE POST FUNC
+makePostBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // GET VALUES
+    let titleVal = postTitle.value;
+    let textVal = postText.value;
+    let imgVal = postImg.value;
+
+    postPost(titleVal, textVal, imgVal);
+})
+
+
+
+// POST POST
+function postPost(titleVal, textVal, imgVal) {
     fetch("https://testapi.io/api/Domantas/resource/Posts", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
         body: JSON.stringify({
-            title: " title",
-            text: "text ",
-            img: "img ",
+            title: titleVal,
+            text: textVal,
+            img: imgVal,
         })
     })
     .then(res => res.json())
@@ -29,5 +47,15 @@ function postPost() {
     .catch(err => console.log(err))
 }
 
-postPost();
 
+
+
+// GET POSTS
+function getPosts (){
+    fetch("	https://testapi.io/api/Domantas/resource/Posts")
+    .then(res => res.json())
+    .then(data => {
+        generatePost(data);
+    })
+    .catch(err => console.log(err))
+}
