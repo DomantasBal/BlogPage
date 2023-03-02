39697f1d6367c1f30c25bf3ae90331959efd6e3c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-02 19:47:18 +0200

    added close open fucntionality login form

--- app/login.js
+++ app/login.js
@@ -2,12 +2,10 @@ const loginIcon = document.querySelector("#login-icon");
 const loginCloseIcon = document.querySelector("#login-close-icon");
 const loginForm = document.querySelector("#login-form");
 
+// LOGIN ICON OPEN - CLOSE FORM
+let countClicks = 0;
 loginIcon.addEventListener("click", (event) => {
   event.preventDefault();
-  loginForm.style.display = "block";
-});
-
-loginCloseIcon.addEventListener("click", (event) => {
-  event.preventDefault();
-  loginForm.style.display = "none";
+  countClicks++;
+  loginForm.style.opacity = countClicks % 2 === 1 ? "1" : "0";
 });
