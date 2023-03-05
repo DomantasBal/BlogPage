647263504afcf1db5f7e40f9c28a807871806cb2
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-05 17:35:42 +0200

    fixed 500 error on post or delete

--- app/blog.js
+++ app/blog.js
@@ -1,45 +1,91 @@
-const form = document.getElementById("new-post-form");
+const newPostForm = document.getElementById("new-post-form");
 const blog = document.getElementById("blog");
 const button = document.querySelector("#new-post-btn");
 
 const API_ENDPOINTS = {
   get: "https://testapi.io/api/Domantas/resource/newPosts",
   post: "https://testapi.io/api/Domantas/resource/newPosts",
-};
-
-// SHOW POSTS ON WEBSITE LOAD
-window.onload = async () => {
-  const posts = await getData(API_ENDPOINTS.get);
-  posts.data.forEach((post) => {
-    blog.innerHTML += postTemplate(post);
-  });
+  put: (id) => `https://testapi.io/api/Domantas/resource/newPosts/${id}`,
+  delete: (id) => `https://testapi.io/api/Domantas/resource/newPosts/${id}`,
 };
 
 // GET METHOD
-function getData(url) {
+const getData = (url) => {
   return fetch(url)
     .then((response) => response.json())
     .then((data) => data)
     .catch((error) => console.log(error));
-}
+};
 
 // POST METHOD
-function postData(url, formData) {
+const createPost = (url, data) => {
   return fetch(url, {
     method: "POST",
+    body: data,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+};
+
+// DELETE POST
+const deletePost = (id) => {
+  const url = API_ENDPOINTS.delete(id);
+  return fetch(url, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      response.status === 204 && document.getElementById(id).remove();
+    })
+    .catch((error) => console.log(error));
+};
+
+// EDIT POST
+const updatePost = (id, updatedData) => {
+  const url = API_ENDPOINTS.put(id);
+  return fetch(url, {
+    method: "PUT",
+    // AR HEADERIAI REIKALINGI TIK PUT PATCH? AR IR DELETE IR POST?
     headers: { "Content-type": "application/json" },
-    body: JSON.stringify(Object.fromEntries(formData.entries())),
+    body: JSON.stringify(updatedData),
   })
     .then((response) => response.json())
     .then((data) => data)
     .catch((error) => console.log(error));
-}
+};
+
+let newStuff = {
+  title: "111",
+  text: "123",
+  img: "upd",
+};
+// updatePost(4, newStuff);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// COLLECTS FORM DATA AND SENDS TO API
+const newPostSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const newPost = await createPost(API_ENDPOINTS.post, formData);
+};
+
+// POST SUBMIT ACTION
+newPostForm.addEventListener("submit", newPostSubmit);
 
 // SINGLE POST TEMPLATE
 const postTemplate = (data) => {
   const x = JSON.stringify(data);
   return `
         <div id=${data.id} class="post">
+        
+        <div class="modify-buttons">
+            <i class="fa-solid fa-pen-to-square edit" onClick=handlePostEdit(${data.id})></i>
+            <i class="fa-sharp fa-solid fa-trash delete" onClick=deletePost(${data.id}) ></i>
+        </div>
+
           <img src="${data.img}" alt="" />
           <div class="post-text-content">
             <h3 class="post-title">${data.title}</h3>
@@ -57,4 +103,12 @@ const postTemplate = (data) => {
           </div>
         </div>
     `;
+};
+
+// SHOW POSTS ON WEBSITE LOAD
+window.onload = async () => {
+  const posts = await getData(API_ENDPOINTS.get);
+  posts.data.forEach((post) => {
+    blog.innerHTML += postTemplate(post);
+  });
 };
